355084e91b1be146f0f6d75bf0e59211322a9a42
dimon-ton <saichon2namoom@gmail.com>
2024-02-17 21:16:13 +0700

    EP.5 complete learning

--- secondProject/myapp/models.py
+++ secondProject/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Tracking(models.Model):
+    name = models.CharField(max_length=100)
+    tel = models.CharField(max_length=100)
+    tracking = models.CharField(max_length=100, null=True, blank=True)
+    other = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return '{} - {} ({})'.format(self.name, self.tel, self.tracking)
--- secondProject/myapp/views.py
+++ secondProject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tracking
 
 # Create your views here.
 
@@ -18,11 +19,13 @@ def contactUs(req):
     return render(req, 'myapp/contactus.html')
 
 def tracking(req):
-    track_list = ['ลุงวิศวกร - A1234',
-                  'สมหญิง - A1235',
-                  'สมชาย - A123'
-                  ]
-    
+    # track_list = ['ลุงวิศวกร - A1234',
+    #               'สมหญิง - A1235',
+    #               'สมชาย - A1236'
+    #               ]
+
+    track_list = Tracking.objects.all()
+    print(track_list)
     context = {'tracking':track_list}
 
     return render(req, "myapp/tracking.html", context)
